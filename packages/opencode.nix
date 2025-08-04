e5d3de7db9f3edd06c6cb395b993472b990c29cc
Mark Baran <mebaran@gmail.com>
2025-08-04 04:20:38 -0700

    Updated hashes

--- packages/opencode.nix
+++ packages/opencode.nix
@@ -1,22 +1,20 @@
-{
-  pkgs,
-  ...
-}:
+{pkgs, ...}:
 pkgs.stdenv.mkDerivation rec {
   pname = "opencode";
-  version = "0.3.113";
+  version = "0.3.126";
 
   src = pkgs.fetchzip {
     url = "https://github.com/sst/opencode/releases/download/v${version}/opencode-${{
-      "x86_64-linux" = "linux-x64";
-      "aarch64-linux" = "linux-arm64";
-      "aarch64-darwin" = "darwin-arm64";
-    }.${pkgs.system}}.zip";
-    sha256 = {
-      "x86_64-linux" = "sha256-8hL2Jd47l/FG0UuSWymc1HnXtqbRKt2pCtjp3/c/nsA=";
-      "aarch64-linux" = "sha256-g5ABOLquhVmEOIGINOxaxYCyDB72O1uoPms7TbOtwb0=";
-      "aarch64-darwin" = "sha256-O6gm6WH9W+I1/INZwvgF2uvjbwmSQNbdCeNpYUUm8Ic=";
-    }.${pkgs.system};
+        "x86_64-linux" = "linux-x64";
+        "aarch64-linux" = "linux-arm64";
+        "aarch64-darwin" = "darwin-arm64";
+      }.${pkgs.system}}.zip";
+    sha256 =
+      {
+        "x86_64-linux" = "05n3i6hlnzz882qwag0c7mnd997psn2ai550jz0w3y423xhwz5bm";
+        "aarch64-linux" = "0h3n7gpq4hj4cdibkzl9y2fr03456dd4km5fb23f6z499pjlaa1y";
+        "aarch64-darwin" = "1qcavbm909nrw4yligypaz47am6pjv7ndqrls8018iffrp1qsjk7";
+      }.${pkgs.system};
     stripRoot = false;
   };
 
